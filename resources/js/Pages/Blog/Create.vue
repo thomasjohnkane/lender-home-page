39c8a15f21c19970dbf20bd024ef8bf7e585d065
Thomas Kane <thomasjohnkane@gmail.com>
2024-11-06 03:11:26 -0600

    Minor changes based on loom videos

--- resources/js/Pages/Blog/Create.vue
+++ resources/js/Pages/Blog/Create.vue
@@ -37,8 +37,8 @@ function submit() {
                                 <label for="username" class="block text-sm/6 font-medium text-gray-900">Post Name</label>
                                 <div class="mt-2">
                                     <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                        <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">Hell of a post</span>
-                                        <input v-model="post.name" type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="friend">
+<!--                                        <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">Hell of a post</span>-->
+                                        <input v-model="post.name" type="text" name="username" id="username" autocomplete="username" class="block flex-1 border-0 bg-transparent py-1.5 pl-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm/6" placeholder="Bring the fire">
                                     </div>
                                 </div>
                             </div>
